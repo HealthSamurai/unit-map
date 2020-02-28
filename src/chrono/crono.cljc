9b9fc0d3bbb82a7c9ee37aa86748da93b53ab9ea
Vladislav Ganshin <ganshinv@gmail.com>
2020-02-28 19:26:53 +0300

    Add crono to chrono

---
+++ src/chrono/crono.cljc
@@ -0,0 +1,44 @@
+(ns chrono.crono
+  (:require [chrono.core :as ch]
+            [chrono.now :as now]))
+
+(def needed-for
+  {:month [:yaer :month]
+   :day [:year :month :day]
+   :hour [:year :month :day :hour]
+   :min [:year :month :day :hour :min]})
+
+(defn next-time-assumption [current-time {every :every at :at}])
+
+(defn next-time
+  ([cfg] (next-time (now/utc) cfg))
+  ([current-time {every :every at :at :as when}]
+   (let [assumptions (map #(merge (select-keys current-time (get needed-for every)) %) (if (map? at) [at] at))]
+     (if (nil? (first (filter #(ch/< current-time %) assumptions)))
+       (ch/+ (first assumptions) {every 1})
+       (first (filter #(ch/< current-time %) assumptions))))))
+
+(comment
+
+  (= {:year 2020 :month 1 :day 1 :hour 12}
+     (next-time {:year 2020 :month 1 :day 1 :hour 11}
+                {:every :day :at {:hour 12}}))
+
+  (= {:year 2020 :month 1 :day 2 :hour 12}
+     (next-time {:year 2020 :month 1 :day 1 :hour 12 :min 10}
+                {:every :day :at {:hour 12}}))
+
+  (= {:year 2020 :month 1 :day 1 :hour 14}
+     (next-time-2 {:year 2020 :month 1 :day 1 :hour 13}
+                  {:every :day :at [{:hour 12}
+                                    {:hour 14}]}))
+
+  (= {:year 2020 :month 1 :day 1 :hour 10 :min 30}
+     (next-time-2 {:year 2020 :month 1 :day 1 :hour 10 :min 13}
+                  {:every :hour :at [{:min 0} {:min 30}]}))
+
+  (= {:year 2020 :month 1 :day 1 :hour 12}
+     (next-time-2 {:year 2020 :month 1 :day 1 :hour 11 :min 43}
+                  {:every :hour :at [{:min 0} {:min 30}]}))
+
+  )
---
+++ test/chrono/crono_test.clj
@@ -0,0 +1,61 @@
+(ns chrono.crono-test
+  (:require [chrono.crono :as sut]
+            [clojure.test :refer :all]))
+
+(deftest crono-test
+
+
+  (is (= {:year 2020 :month 1 :day 1 :hour 12}
+         (sut/next-time {:year 2020 :month 1 :day 1 :hour 11}
+                    {:every :day :at {:hour 12}})))
+
+  (is (= {:year 2020 :month 1 :day 2 :hour 12}
+         (sut/next-time {:year 2020 :month 1 :day 1 :hour 12 :min 10}
+                    {:every :day :at {:hour 12}})))
+
+  (is (= {:year 2020 :month 1 :day 1 :hour 14}
+         (sut/next-time {:year 2020 :month 1 :day 1 :hour 13}
+                      {:every :day :at [{:hour 12}
+                                        {:hour 14}]})))
+
+  (is (= {:year 2020 :month 1 :day 1 :hour 10 :min 30}
+         (sut/next-time {:year 2020 :month 1 :day 1 :hour 10 :min 13}
+                      {:every :hour :at [{:min 0} {:min 30}]})))
+
+  (is (= {:year 2020 :month 1 :day 1 :hour 12}
+         (sut/next-time {:year 2020 :month 1 :day 1 :hour 11 :min 43}
+                      {:every :hour :at [{:min 0} {:min 30}]})))
+
+  (is (= {:year 2020 :month 1 :day 1 :hour 12 :min 10}
+         (sut/next-time {:year 2020 :month 1 :day 1 :hour 12 :min 7}
+                    {:every :hour :at [{:min 0}
+                                       {:min 5}
+                                       {:min 10}
+                                       {:min 15}
+                                       {:min 20}
+                                       {:min 25}
+                                       {:min 30}
+                                       {:min 35}
+                                       {:min 40}
+                                       {:min 45}
+                                       {:min 50}
+                                       {:min 55}]})))
+
+  (is (= {:year 2020 :month 1 :day 1 :hour 11}
+         (sut/next-time {:year 2020 :month 1 :day 1 :hour 10 :min 55}
+                    {:every :hour :at [{:min 0}
+                                       {:min 5}
+                                       {:min 10}
+                                       {:min 15}
+                                       {:min 20}
+                                       {:min 25}
+                                       {:min 30}
+                                       {:min 35}
+                                       {:min 40}
+                                       {:min 45}
+                                       {:min 50}
+                                       {:min 55}]})))
+
+
+
+  )
