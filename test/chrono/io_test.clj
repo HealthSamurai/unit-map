782fcbfb210535d95b92a3442bbbc72a73be0a60
Alexander Ushanov <flawless@last-try.org>
2020-04-23 03:49:44 +0300

    Add from-epoch to-epoch. Support only positive unixtime

--- test/chrono/io_test.clj
+++ test/chrono/io_test.clj
@@ -163,7 +163,7 @@
 
 (deftest from-epoch-test
   (is (= {:day 22, :month 4, :year 2020, :sec 40, :min 49, :hour 1} (sut/from-epoch 1587520180)))
-  (is (= {:day 1, :month 1, :year 1970, :sec 0, :min 0, :hour 0} (sut/from-epoch 0))))
+  (is (= {:day 1, :month 1, :year 1970} (sut/from-epoch 0))))
 
 (deftest to-epoch-test
   (is (= 1587520180 (sut/to-epoch {:day 22, :month 4, :year 2020, :sec 40, :min 49, :hour 1})))
