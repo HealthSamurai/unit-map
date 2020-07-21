bcace7d505230c483861f04b65d3470cebc74364
KGOH <kgofhedgehogs@gmail.com>
2020-07-21 02:40:27 +0200

    Format code

--- test/chrono/new_ops_test.clj
+++ test/chrono/new_ops_test.clj
@@ -4,65 +4,65 @@
             [clojure.test :as t]))
 
 (t/deftest range-test
-  (def base60   (:min   (sut/rules {})))
+  (def base60   (:min  (sut/rules {})))
   (def months   (:month (sut/rules {})))
-  (def years    (:year  (sut/rules {})))
+  (def years    (:year (sut/rules {})))
   (def am-hours (:hour (sut/rules ^:am-pm{})))
 
-    (t/testing "process-sequence"
-      (matcho/match (sut/process-sequence base60)
-                    [{:start 0, :step 1, :end 59}])
+  (t/testing "process-sequence"
+    (matcho/match (sut/process-sequence base60)
+                  [{:start 0, :step 1, :end 59}])
 
-      (matcho/match (sut/process-sequence months)
-                    [:jan :feb :mar :apr :may :jun :jul :aug :sep :oct :nov :dec])
+    (matcho/match (sut/process-sequence months)
+                  [:jan :feb :mar :apr :may :jun :jul :aug :sep :oct :nov :dec])
 
-      (matcho/match (sut/process-sequence years)
-                    [{:start ##-Inf, :step 1, :end -1}
-                     {:start 1, :step 1, :end ##Inf}])
+    (matcho/match (sut/process-sequence years)
+                  [{:start ##-Inf, :step 1, :end -1}
+                   {:start 1, :step 1, :end ##Inf}])
 
-      (matcho/match (sut/process-sequence am-hours)
-                    [12 {:start 1, :step 1, :end 11}]))
+    (matcho/match (sut/process-sequence am-hours)
+                  [12 {:start 1, :step 1, :end 11}]))
 
-    (t/testing "get-next"
-      (matcho/match (->> 0
-                         (iterate (partial sut/get-next (sut/process-sequence base60)))
-                         (take-while some?))
-                    (range 60))
+  (t/testing "get-next"
+    (matcho/match (->> 0
+                       (iterate (partial sut/get-next (sut/process-sequence base60)))
+                       (take-while some?))
+                  (range 60))
 
-      (matcho/match (->> :jan
-                         (iterate (partial sut/get-next (sut/process-sequence months)))
-                         (take-while some?))
-                    [:jan :feb :mar :apr :may :jun :jul :aug :sep :oct :nov :dec])
+    (matcho/match (->> :jan
+                       (iterate (partial sut/get-next (sut/process-sequence months)))
+                       (take-while some?))
+                  [:jan :feb :mar :apr :may :jun :jul :aug :sep :oct :nov :dec])
 
-      (matcho/match (->> 1970
-                         (iterate (partial sut/get-next (sut/process-sequence years)))
-                         (take 51))
-                    (range 1970 2021))
+    (matcho/match (->> 1970
+                       (iterate (partial sut/get-next (sut/process-sequence years)))
+                       (take 51))
+                  (range 1970 2021))
 
-      (matcho/match (->> 12
-                         (iterate (partial sut/get-next (sut/process-sequence am-hours)))
-                         (take-while some?))
-                    [12 1 2 3 4 5 6 7 8 9 10 11]))
+    (matcho/match (->> 12
+                       (iterate (partial sut/get-next (sut/process-sequence am-hours)))
+                       (take-while some?))
+                  [12 1 2 3 4 5 6 7 8 9 10 11]))
 
-    (t/testing "get-prev"
-      (matcho/match (->> 59
-                         (iterate (partial sut/get-prev (sut/process-sequence base60)))
-                         (take-while some?))
-                    (range 59 -1 -1))
+  (t/testing "get-prev"
+    (matcho/match (->> 59
+                       (iterate (partial sut/get-prev (sut/process-sequence base60)))
+                       (take-while some?))
+                  (range 59 -1 -1))
 
-      (matcho/match (->> :dec
-                         (iterate (->> months
-                                       sut/process-sequence
-                                       (partial sut/get-prev)))
-                         (take-while some?))
-                    [:dec :nov :oct :sep :aug :jul :jun :may :apr :mar :feb :jan])
+    (matcho/match (->> :dec
+                       (iterate (->> months
+                                     sut/process-sequence
+                                     (partial sut/get-prev)))
+                       (take-while some?))
+                  [:dec :nov :oct :sep :aug :jul :jun :may :apr :mar :feb :jan])
 
-      (matcho/match (->> 1970
-                         (iterate (partial sut/get-prev (sut/process-sequence years)))
-                         (take 51))
-                    (range 1970 1920))
+    (matcho/match (->> 1970
+                       (iterate (partial sut/get-prev (sut/process-sequence years)))
+                       (take 51))
+                  (range 1970 1920))
 
-      (matcho/match (->> 11
-                         (iterate (partial sut/get-prev (sut/process-sequence am-hours)))
-                         (take-while some?))
-                    [11 10 9 8 7 6 5 4 3 2 1 12])))
+    (matcho/match (->> 11
+                       (iterate (partial sut/get-prev (sut/process-sequence am-hours)))
+                       (take-while some?))
+                  [11 10 9 8 7 6 5 4 3 2 1 12])))
